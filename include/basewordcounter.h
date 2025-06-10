9fa05e592dd9b16589d4e4fbea8e6186165f22c3
LingNc <1020449099@qq.com>
2025-06-10 15:24:36 +0800

    重构BaseWordCounter类，添加search_word方法并更新get_ASL实现，确保一些实现由基类完成，子类只需要实现核心部分

--- include/basewordcounter.h
+++ include/basewordcounter.h
@@ -3,28 +3,40 @@
 
 #include "basetype.h"
 
-class BaseWordCounter {
+class BaseWordCounter{
 public:
-    virtual ~BaseWordCounter() = default;
+    struct Ret{
+        // 查找结果
+        bool isFound;
+        // 查找时间，单位微秒
+        int64_t time_us;
+        // 查找词频
+        size_t wordFreq;
+        // 本次比较次数
+        size_t comparisons;
+    };
+    virtual ~BaseWordCounter()=default;
+
+    // 载入基础单词表
+    virtual void load(const SqList<std::string> &baseTable);
 
     // 插入或更新一个单词的频率
     virtual void insert_word(const std::string& word) = 0;
 
-    // 查找一个单词的频率，返回是否找到，并输出比较次数
-    virtual bool search_word(const std::string& word, int& frequency, int& comparisons) = 0;
-
     // 获取频率表
-    virtual FreqTable& get_frequency_table() const = 0;
+    virtual const FreqTable& get_frequency_table() const = 0;
+
+    // 查找一个单词的频率，返回花费时间和词频
+    Ret search_word(const std::string& word);
 
     // 获取ASL（查找成功的平均比较次数）
-    virtual double get_ASL() const = 0;
-
+    double get_ASL() const;
+protected:
+    // 查找单词词频，返回是否(成功,词频)
+    virtual Ret _search_word(const std::string &word)=0;
 private:
-    // 获取插入过程中总比较次数（用于ASL计算）
-    virtual int get_total_comparisons() const = 0;
-
-    // 获取成功查找的次数（用于ASL计算）
-    virtual int get_success_count() const = 0;
+    size_t _totalCompares=0;
+    size_t _successCounts=0;
 };
 
 #endif // BASE_WORD_COUNTER_H
