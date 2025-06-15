8d335b041469ffc9c4aab4995bda373a94a80218
LingNc <1020449099@qq.com>
2025-06-15 18:26:05 +0800

    修复顺序表查找的 _search_word 函数中的逻辑错误，确保二分查找时正确判断等于情况，处理正确的二分查找比较次数。 修复基类中当第一次查找失败的时候，必定除零，修改的返回值，返回-1表示计算失败

--- include/binarysqlist.h
+++ include/binarysqlist.h
@@ -7,5 +7,6 @@ public:
     virtual void insert_word(const std::string &word) override;
     virtual string name() const override;
 private:
-    virtual Pair<size_t, size_t> find_word(const string &word) override;
+    virtual Pair<size_t,size_t> find_word(const string &word) override;
+    virtual Ret _search_word(const string &word) override;
 };
